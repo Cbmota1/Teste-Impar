7f880e4f88c911e6b746356bd82d2276c76633ff
Caio Mota <mota.caiobatista@gmail.com>
2024-09-28 08:24:15 -0300

    correct typo

--- src/componentes/ListaPersonagens/index.tsx
+++ src/componentes/ListaPersonagens/index.tsx
@@ -4,20 +4,6 @@ import axios from 'axios';
 export interface Personagem {
   name: string;
   height: string;
-  mass: string;
-  hair_color: string;
-  skin_color: string;
-  eye_color: string;
-  birth_year: string;
-  gender: string;
-  homeworld: string;
-  films: string;
-  species: string;
-  vehicles: string;
-  starships: string;
-  created: string;
-  edited: string;
-  url: string;
 }
 
 export const useListaDePersonagens = (): Personagem[] => {
